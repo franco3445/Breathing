c3de929d671b97b576708e6137268d3e65588e52
gFranco <gfranco@velocitymsc.com>
2024-10-30 17:46:44 -0500

    move to bottom of main screen

--- main.pl
+++ main.pl
@@ -9,13 +9,21 @@ my $colorOptions = [0, 'red', 20, 'orange' , 40, 'yellow', 60, 'green', 80, 'blu
 
 my $mainWindow = MainWindow->new;
 
+my $screenHeight = $mainWindow->screenheight - 150;
+my $screenWidth = $mainWindow->screenwidth;
+my $windowGeometry = "x50+0+$screenHeight";
+
+$mainWindow->geometry($screenWidth . $windowGeometry);
+
 $mainWindow->Label(-text => 'Focus on your breathing...')->pack;
 
 $mainWindow->ProgressBar(
-    -from   => 0,
-    -to     => 100,
-    -blocks => 100,
-    -colors => $colorOptions,
+    -blocks   => 100,
+    -colors   => $colorOptions,
+    -from     => 0,
+    -gap      => 0,
+    -to       => 100,
+    -length   => $screenWidth,
     -variable => \$updatePercentage,
 )->pack;
 
@@ -24,7 +32,7 @@ $mainWindow->Button(
     -command => sub { exit },
 )->pack;
 
-$mainWindow->after(1000, (\&breath(5)));
+$mainWindow->after(1000, (\&breath(1)));
 
 MainLoop;
 
@@ -32,10 +40,10 @@ sub breath {
     my $step = shift;
     my $direction = 'up';
     while ($direction eq 'up'){
-        for (1..20) {
+        for (1..100) {
             $updatePercentage += $step;
             $mainWindow->update;
-            select(undef, undef, undef, 0.1);
+            select(undef, undef, undef, 0.01);
         }
         select(undef, undef, undef, 0.3);
         $direction = 'down';
